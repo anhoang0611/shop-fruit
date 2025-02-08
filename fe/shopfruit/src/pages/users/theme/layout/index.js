c7089dc23fce8284fd460a0529fe12ab0addc6f4
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-09 02:07:17 +0700

    hoan thanh categories va header

--- fe/shopfruit/src/pages/users/theme/layout/index.js
+++ fe/shopfruit/src/pages/users/theme/layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../header';
 import Footer from '../footer';
-import Homepages from '../../homepages';
+
 const MasterLayout = ({ children, ...props }) => {
     return (
         <div {...props}>
