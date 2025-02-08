edb694b24801e0538057573393fcee54f9169b4d
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-08 18:11:35 +0700

    toi uu footer va responsive

--- fe/shopfruit/src/pages/users/theme/footer/index.js
+++ fe/shopfruit/src/pages/users/theme/footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='container'>
             <div className='row'>
 
-                <div className='col-lg-3'>
+                <div className='col-lg-3 col-md-6 col-sm-12 col-xs-12'>
                     <div className='footer-about'>
                         <h1 className='footer-about-logo'>TiGi Shop</h1>
                         <ul>
@@ -18,7 +18,7 @@ const Footer = () => {
 
                     </div>
                 </div>
-                <div className='col-lg-6'>
+                <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
                     <div className='footer-widget'>
                         <h6>Cửa hàng</h6>
                         <ul>
@@ -51,12 +51,12 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className='col-lg-3'>
+                <div className='col-lg-3 col-md-12 col-sm-12 col-xs-12'>
                     <div className='footer-widget'>
                         <h6>Khuyến mãi & ưu đãi</h6>
                         <p>Đăng ký để nhận thông tin khuyến mãi</p>
                         <form action='#'>
-                            <div>
+                            <div className='input-group'>
                                 <input type="text" placeholder='Nhập email' />
                                 <button type='submit' className='button-submit'>Đăng ký</button>
 
