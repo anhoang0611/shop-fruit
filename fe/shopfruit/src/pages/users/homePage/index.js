48c364ab584c330377663857c642abd2ea8f79bc
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-10 19:00:44 +0700

    tao tablist hien thi cac san pham

--- fe/shopfruit/src/pages/users/homePage/index.js
+++ fe/shopfruit/src/pages/users/homePage/index.js
@@ -1,19 +1,25 @@
+//libraries
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import './style.scss';
-
+//categories images
 import cam from '../../../assets/users/images/categories/cam1.jpg';
 import tao from '../../../assets/users/images/categories/tao.jpg';
 import buoi from '../../../assets/users/images/categories/buoi.jpg';
 import le from '../../../assets/users/images/categories/le.jpg';
 import nho from '../../../assets/users/images/categories/nho.jpg';
+//featured products images
+import thitbo from '../../../assets/users/images/features/thit-bo.jpg';
+import chuoi from '../../../assets/users/images/features/chuoi.jpg';
+
 
 
 const Homepages = () => {
+    //responsive 
     const responsive = {
         superLargeDesktop: {
-            // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 3000 },
             items: 5
         },
@@ -31,9 +37,11 @@ const Homepages = () => {
         }
     };
 
+    //slide items
     const slideItems = [
         {
             bgImg: cam,
+
             name: 'Cam tươi',
         },
         {
@@ -52,9 +60,82 @@ const Homepages = () => {
             bgImg: nho,
             name: 'Nho',
         }
-    ]
+    ];
+    const featuredProducts = {
+        all: {
+            title: 'Toàn bộ',
+            products: [
+                {
+                    img: thitbo,
+                    name: 'Thịt bò nạc',
+                    price: 20000,
+                },
+                {
+                    img: chuoi,
+                    name: 'Chuối',
+                    price: 14000,
+                }
+            ]
+        },
+        freshMeat: {
+            title: 'Thịt tươi',
+            products: [
+                {
+                    img: thitbo,
+                    name: 'Thịt bò nạc',
+                    price: 20000,
+                }
+
+            ]
+        }
+    };
+
+    const renderFeaturedProducts = (data) => {
+        const tabLists = [];
+        const tabPanels = [];
+
+        Object.keys(data).forEach((key, index) => {
+            tabLists.push(
+                <Tab key={index}>{data[key].title}</Tab>
+            )
+
+
+            const tabPanel = [];
+
+            data[key].products.forEach((product, prodIndex) => (
+                tabPanel.push(
+                    <div key={prodIndex}>{product.name}</div>
+                )
+            ));
+            tabPanels.push(tabPanel);
+        });
+        return (
+            <Tabs>
+                <TabList>
+                    {tabLists}
+                </TabList>
+                {
+                    tabPanels.map((item, index) => (
+                        <TabPanel key={index}>
+                            <div className='row'>
+                                {item}
+
+                            </div>
+                        </TabPanel>
+
+                    ))
+                }
+            </Tabs>
+        )
+
+
+
+    };
+
+
     return (
         <>
+            {/* slide categories start */}
             <div className='container container-categories-slide'>
                 <Carousel responsive={responsive} className='categories-slide'>
                     {
@@ -74,8 +155,21 @@ const Homepages = () => {
                 </Carousel>
 
             </div>
+            {/* slide categories end */}
+
+            {/* featured products start */}
+            <div className='container'>
+                <div className='featured'>
+                    <div className='section-title'>
+                        <h2>Sản phẩm nổi bật</h2>
+                    </div>
+                    {renderFeaturedProducts(featuredProducts)}
+                </div>
+
+            </div>
 
         </>
+
     )
 }
 
