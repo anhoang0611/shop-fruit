c2e336abe8ddf7bb7311d0319f05d7fcf6d49c11
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-14 17:40:05 +0700

    tao san pham tuong tu (chi tiet san pham)

--- fe/shopfruit/src/pages/users/productPageDetail/index.js
+++ fe/shopfruit/src/pages/users/productPageDetail/index.js
@@ -6,6 +6,8 @@ import buoi2 from "../../../assets/users/images/categories/buoi2.jpg";
 import buoi3 from "../../../assets/users/images/categories/buoi3.jpg";
 import { AiOutlineEye, AiOutlineFacebook, AiOutlineTwitter, AiOutlineInstagram } from "react-icons/ai";
 import { formatter } from "../../../utils/formatter";
+import { ProductCard } from '../../../component';
+import { featuredProducts } from '../../../utils/common';
 const ProductPageDetail = () => {
 
     const img = [buoi, buoi2, buoi3];
@@ -56,6 +58,98 @@ const ProductPageDetail = () => {
                             </li>
                         </ul>
                     </div>
+                </div>
+
+                <div className='product-detail-tab'>
+                    <h4>Thông tin chi tiết</h4>
+                    <div>
+                        <ul>
+                            <li>
+                                <p>
+                                    Rau củ chứa rất nhiều vitamin và chất dinh dưỡng nên mang đến
+                                    rất nhiều lợi ích cho sức khỏe con người.&nbsp;
+                                </p>
+                            </li>
+                            <li>
+                                <p>Hỗ trợ giảm cân</p>
+                            </li>
+                            <li>
+                                <p>
+                                    Giảm nguy cơ mắc các bệnh về tim mạch, béo phì và cả ung thư
+                                </p>
+                            </li>
+                            <li>
+                                <p>Tăng cường sức đề kháng của cơ thể</p>
+                            </li>
+                            <li>
+                                <p>Cải thiện thị lực</p>
+                            </li>
+                            <li>
+                                <p>Điều hòa đường huyết</p>
+                            </li>
+                            <li>
+                                <p>Giảm cholesterol trong máu</p>
+                            </li>
+                            <li>
+                                <p>Điều hòa huyết áp</p>
+                            </li>
+                        </ul>
+                        <p>
+                            <br />
+                            <strong>Cách chọn rau củ tươi ngon</strong>
+                        </p>
+                        <ul>
+                            <li>
+                                <p>
+                                    Dựa vào hình dáng bên ngoài: Nên ưu tiên chọn các loại rau củ
+                                    có phần vỏ không có các vết sâu, cuống lá không bị nhũn, thâm
+                                    đen. Tránh chọn những loại quả có vẻ ngoài to tròn, căng lớn,
+                                    bởi đây có thể là những quả đã bị tiêm thuốc, không an toàn
+                                    cho sức khỏe.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    Dựa vào màu sắc: Màu sắc của các loại rau củ thường tươi mới,
+                                    không có các vết xước, héo hay quá đậm màu. Các loại củ có màu
+                                    quá xanh hoặc quá bóng sẽ không hẳn là an toàn cho sức khỏe
+                                    người dùng.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    Dựa vào mùi hương: Mùi hương tự nhiên của các loại rau củ sẽ
+                                    là mùi đặc trưng theo từng loại chứ không phải là mùi hắc,
+                                    nồng khó chịu. Nếu các loại củ bạn đang chọn có mùi hóa chất
+                                    hãy ngưng sử dụng ngay.
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                    Dựa vào cảm nhận khi cầm: Những loại củ quả cầm chắc tay, kích
+                                    thước vừa phải, không quá to sẽ thường ngon hơn những loại to
+                                    lớn nhưng khối lượng nhẹ. Một số loại rau củ bạn chỉ nên chọn
+                                    những quả nhỏ, đều tay sẽ ngon hơn.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                </div>
+                <div className='section-title'>
+                    <h2>Sản phẩm tương tự</h2>
+
+                </div>
+                <div className='row'>
+                    {
+                        featuredProducts.all.products.map((item, index) => (
+                            <div key={index} className='col-lg-3 col-md-4 col-sm-6'>
+                                <ProductCard img={item.img} name={item.name} price={item.price} />
+                            </div>
+                        ))
+                    }
+
                 </div>
             </div>
         </>
